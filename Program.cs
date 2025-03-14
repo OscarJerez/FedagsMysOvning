ae59be49a1736150a461d2ca7de5adab7e2ee6ad
OscarSystemDeveloper <oscj87@gmail.com>
2025-03-14 10:24:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FedagsMysÖvning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
