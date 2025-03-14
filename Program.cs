acc70a27dae899341ba36322a2fbeb2bbcb8535e
OscarSystemDeveloper <oscj87@gmail.com>
2025-03-14 10:49:24 +0100

    Anagram kod

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
 ﻿namespace FedagsMysÖvning
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
     {
-        static void Main(string[] args)
+        public static List<List<string>> GrupperaAnagram(List<string> ordLista)
         {
-            Console.WriteLine("Hello, World!");
+            // Dictionary där nyckeln är ordets bokstäver i alfabetisk ordning
+            var anagramDictionary = new Dictionary<string, List<string>>();
+
+            foreach (var ord in ordLista)
+            {
+                // Sortera bokstäverna i ordet för att skapa en unik nyckel
+                string sorteradNyckel = new string(ord.OrderBy(c => c).ToArray());
+
+                // Om nyckeln redan finns, lägg till ordet i listan, annars skapa en ny lista
+                if (!anagramDictionary.ContainsKey(sorteradNyckel))
+                {
+                    anagramDictionary[sorteradNyckel] = new List<string>();
+                }
+                anagramDictionary[sorteradNyckel].Add(ord);
+            }
+
+            // Returnera alla grupper av anagram
+            return anagramDictionary.Values.ToList();
+        }
+
+        static void Main()
+        {
+            var input = new List<string> { "listen", "silent", "enlist", "rat", "tar", "art", "evil", "vile", "live" };
+            var resultat = GrupperaAnagram(input);
+
+            // Skriv ut resultatet
+            foreach (var grupp in resultat)
+            {
+                Console.WriteLine(string.Join(", ", grupp));
+            }
         }
     }
+
 }
