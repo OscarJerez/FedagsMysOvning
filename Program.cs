b1dd57ece4e6e26104c78cfcf542df4698b63835
OscarSystemDeveloper <oscj87@gmail.com>
2025-03-14 11:05:00 +0100

    Kod till Hitta Första Unika Tecknet

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
 ﻿namespace FedagsMysÖvning
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+
+    namespace FörstaUnikaTecknet
     {
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                // Fördefinierade strängar att testa
+                var texterViUndersöker = new List<string> { "swiss", "racecars", "aabb" };
+
+                // Kör och skriv ut resultat för varje sträng
+                foreach (var text in texterViUndersöker)
+                {
+                    char resultat = HittaFörstaUnikaTecknet(text);
+                    Console.WriteLine($"För strängen \"{text}\" är det första unika tecknet: '{resultat}'");
+                }
+            }
+
+            static char HittaFörstaUnikaTecknet(string analyseradText)
+            {
+                if (string.IsNullOrEmpty(analyseradText))
+                {
+                    return '_';
+                }
+
+                var teckenFrekvens = new Dictionary<char, int>();
+
+                foreach (var tecken in analyseradText)
+                {
+                    if (teckenFrekvens.ContainsKey(tecken))
+                    {
+                        teckenFrekvens[tecken]++;
+                    }
+                    else
+                    {
+                        teckenFrekvens[tecken] = 1;
+                    }
+                }
+
+                foreach (var tecken in analyseradText)
+                {
+                    if (teckenFrekvens[tecken] == 1)
+                    {
+                        return tecken;
+                    }
+                }
+
+                return '_';
+            }
         }
     }
+
 }
